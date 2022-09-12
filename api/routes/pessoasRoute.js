514287ffa19d20c7d28566890eccf3513f8a4edb
nicollas <in.nicollas@gmail.com>
2022-09-12 16:37:04 -0300

    primeiro commit

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+const { route } = require('..')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+
+
+module.exports = router
